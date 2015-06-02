ad4878d3aba932d876738fc94fdc3e6ac73c2ac3
lynnug <lynnasiimwe@gmail.com>
2015-06-02 15:42:05 +0300

    first commit

---
+++ gsm_send_sms.py
@@ -0,0 +1,36 @@
+from pygsm import GsmModem
+
+class overmodem:
+     def __init__(self,port):
+        self.port=port
+    def statusmsg( self, input_string ):
+        if self.verbose:
+            print "[ STATUS ] " + input_string
+    def errormsg( self, input_string ):
+        if self.verbose:
+            print "[ ERROR ] " + input_string
+                    
+    def sms(self , phone_number,text):
+        self.modem.wait_for_network()
+        was_sent = self.modem.send_sms(
+            		phone_number,
+            		text)
+        return was_sent
+  
+    
+    def gsm_log(self, modem, str, level):
+        self.debug("%s: %s" % (level, str))
+   
+    def start_modem(self):
+        self.statusmsg("Connecting and booting via pyGSM...")
+    	result=False
+    	try:
+    		result=True
+    		
+    		self.modem = GsmModem(logger=self.gsm_log, port=self.port)
+    		self.modem.boot()
+    	except Exception as e:
+    		self.statusmsg(e.message)
+    	
+        	
+    	return result
